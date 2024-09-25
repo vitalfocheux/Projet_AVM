2454a2dec7585507d8dfe3c3df1aba9600062cdf
lleuthar <leo.leuthardt@edu.univ-fcomte.fr>
2024-09-25 11:02:15 +0200

    Fixed warnings, need to fix the grammar to fix compilation also add test ressources folder

--- src/main/java/fr/m1comp5/Main.java
+++ src/main/java/fr/m1comp5/Main.java
@@ -1,9 +1,11 @@
 package fr.m1comp5;
+import fr.m1comp5.LexerParserGenerator.MiniJajaParser.MiniJajaParser;
+
+import java.io.FileReader;
 
 public class Main
 {
     public static void main(String[] args)
     {
-
     }
 }
