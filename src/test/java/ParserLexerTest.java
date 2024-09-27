de2f41262491fb2d473fcce81007f9da70810c0a
hrgt <rougetethugo@gmail.com>
2024-09-28 01:34:55 +0200

    Tryout test

--- src/test/java/ParserLexerTest.java
+++ src/test/java/ParserLexerTest.java
@@ -1,19 +1,24 @@
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 import fr.m1comp5.LexerParserGenerator.MiniJajaParser.MiniJaja;
 import fr.m1comp5.LexerParserGenerator.MiniJajaParser.ParseException;
 
 import java.io.File;
+import java.io.StringReader;
 
-public class ParserLexerTest
-{
-    final String pathToResources = "target/classes/data/";
+import static org.junit.Assert.*;
 
+public class ParserLexerTest {
     @Test
-    public void basic() throws ParseException
-    {
-        MiniJaja mjjparser = MiniJaja.getInstance(pathToResources + "1.mjj");
-        mjjparser.classe();
+    public void testValidInput() {
+        String input = "class Test { final int x = 10; main{ int x = 3+4;} }";
+        MiniJaja parser = new MiniJaja(new StringReader(input));
+
+        try {
+            parser.classe();
+            assertTrue(true);
+        } catch (ParseException e) {
+            fail("Parse exception was thrown for valid input" + e.getMessage());
+        }
     }
 }
