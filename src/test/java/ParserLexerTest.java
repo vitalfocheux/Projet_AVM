e17aa1af8a5199ae96248abace682a0cc1b6c8bf
hrgt <rougetethugo@gmail.com>
2024-09-28 01:50:13 +0200

    Tryout test

--- src/test/java/ParserLexerTest.java
+++ src/test/java/ParserLexerTest.java
@@ -8,17 +8,36 @@ import java.io.StringReader;
 
 import static org.junit.Assert.*;
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class ParserLexerTest {
     @Test
     public void testValidInput() {
-        String input = "class Test { final int x = 10; main{ int x = 3+4;} }";
-        MiniJaja parser = new MiniJaja(new StringReader(input));
+        String path = "target/classes/data/1.mjj";
+        displayFileContents(path);
+        MiniJaja parser = null;
 
         try {
+            parser = MiniJaja.getInstance(path);
+            assert parser != null;
             parser.classe();
             assertTrue(true);
         } catch (ParseException e) {
-            fail("Parse exception was thrown for valid input" + e.getMessage());
+            fail("Parse exception was thrown for valid input " + e.getMessage());
+        }
+    }
+
+    private void displayFileContents(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            System.out.println("Contents of " + filePath + ":");
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
         }
     }
 }
