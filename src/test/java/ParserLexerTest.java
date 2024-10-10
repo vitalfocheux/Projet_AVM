e878960ab227af041780992eeae48b3320038b37
lleuthar <leo.leuthardt@edu.univ-fcomte.fr>
2024-10-10 16:48:18 +0200

    Add nodes

--- src/test/java/ParserLexerTest.java
+++ src/test/java/ParserLexerTest.java
@@ -1,12 +1,14 @@
 import fr.m1comp5.LexerParserGenerator.MiniJajaParser.MiniJaja;
 import fr.m1comp5.LexerParserGenerator.MiniJajaParser.ParseException;
 
+import fr.m1comp5.LexerParserGenerator.MiniJajaParser.SimpleNode;
 import org.junit.Test;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import static org.junit.Assert.*;
 
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -29,7 +31,16 @@ public class ParserLexerTest
         try(InputStream is = new FileInputStream(filepath)){
             Reader reader = new InputStreamReader(is);
             MiniJaja mjjparser = new MiniJaja(reader);
-            mjjparser.classe();
+            try
+            {
+                System.out.println(filepath);
+                SimpleNode n = mjjparser.start();
+                n.dump("");
+            }
+            catch (ParseException e)
+            {
+                System.out.println(e.getMessage());
+            }
         }catch (FileNotFoundException e){
             System.out.println("File: "+filepath+" not found");
         }catch (IOException e){
