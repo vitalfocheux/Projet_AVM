9b89ee801e6715ccaa23363288b69aef30a3f6b5
majdouma <malak.majdoub@etu.univ-grenoble-alpes.f>
2024-12-07 14:19:12 +0100

    deleted reps

--- Debug/LoggerListener.java
+++
@@ -1,5 +0,0 @@
-package fr.m1comp5.Debug;
-
-public interface LoggerListener {
-    void onLog(String message, AppLogger.MessageLevel level);
-}
--- Interpreter/src/main/java/fr/m1comp5/Debug/PositionError.java
+++
@@ -1,6 +0,0 @@
-package fr.m1comp5.Debug;
-
-public interface PositionError {
-    int getLine();
-    int getColumn();
-}
