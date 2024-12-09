441989e8ee8df46e7505b83b12eef3e74166ffb3
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-12-09 21:28:49 +0100

    Nouveaux tests pour la classe Memory

--- Memory/src/test/java/fr/m1comp5/MemoryTest.java
+++ Memory/src/test/java/fr/m1comp5/MemoryTest.java
@@ -49,6 +49,105 @@ public class MemoryTest {
         });
     }
 
+    @Test
+    public void assignValueVar() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", 1, ObjectType.INT);
+        mem.assignValue("x", 2);
+        Assertions.assertEquals(mem.getVal("x"), 2);
+    }
 
+    @Test
+    public void assignValueNoneVarWasDecl(){
+        mem.getSymbolTable().newScope();
+        Assertions.assertThrows(StackException.class, () -> {
+            mem.assignValue("x", 2);
+        });
+    }
+
+    @Test
+    public void assignValueDoesntExist() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("y", 1, ObjectType.INT);
+        Assertions.assertThrows(SymbolTableException.class, () -> {
+            mem.assignValue("x", 2);
+        });
+    }
+
+    @Test
+    public void assignValueNotInitialised() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", null, ObjectType.OMEGA);
+        mem.assignValue("x", 2);
+    }
+
+    @Test
+    public void assignValueWithNull() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", null, ObjectType.OMEGA);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("x", null);
+        });
+    }
+
+    @Test
+    public void assignValueAnIntToBool() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", 1, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("x", true);
+        });
+    }
+
+    //TODO: Pas sur que ça sois normal de pouvoir faire ça à revoir
+//    @Test
+//    public void assignValueAnIntToBoolOmega() throws SymbolTableException, StackException, HeapException {
+//        mem.getSymbolTable().newScope();
+//        mem.declVar("x", "OMEGA", ObjectType.INT);
+//        mem.assignValue("x", true);
+//        System.err.println(mem.getVal("x").getClass());
+//    }
+
+    @Test
+    public void getValCst() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declCst("x", 1, ObjectType.INT);
+        Assertions.assertEquals(mem.getVal("x"), 1);
+    }
+
+    @Test
+    public void assignCst() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declCst("x", 1, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("x", 2);
+        });
+    }
+
+    @Test
+    public void assignCstDoesntExist() {
+        mem.getSymbolTable().newScope();
+        Assertions.assertThrows(StackException.class, () -> {
+            mem.assignValue("x", 2);
+        });
+    }
+
+    @Test
+    public void assignCstNotInitialised() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declCst("x", null, ObjectType.OMEGA);
+        mem.assignValue("x", 2);
+        Assertions.assertEquals(mem.getVal("x"), 2);
+        Assertions.assertEquals(mem.getType("x"), ObjectType.OMEGA);
+    }
+
+    @Test
+    public void assignCstWithNull() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declCst("x", 1, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("x", null);
+        });
+    }
 
 }
