1f66e1912effe2074295a859f16129f3febba6a2
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2024-12-12 19:28:56 +0100

    Encore des tests pour la classe Memory

--- Memory/src/test/java/fr/m1comp5/MemoryTest.java
+++ Memory/src/test/java/fr/m1comp5/MemoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Map;
+
 public class MemoryTest {
 
     Memory mem;
@@ -148,6 +151,164 @@ public class MemoryTest {
         Assertions.assertThrows(RuntimeException.class, () -> {
             mem.assignValue("x", null);
         });
+    }
+
+    @Test
+    public void assignMeth() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declMeth("main", "methmain", ObjectType.VOID);
+        Assertions.assertEquals(mem.getType("main"), ObjectType.VOID);
+        Assertions.assertEquals(mem.getNature("main"), ObjectNature.METH);
+        Assertions.assertEquals(mem.getVal("main"), "methmain");
+        MemoryObject mo = mem.assignValue("main", "methmain2");
+        Assertions.assertEquals(mo.getId(), "main");
+        Assertions.assertEquals(mo.getNature(), ObjectNature.METH);
+        Assertions.assertEquals(mo.getType(), ObjectType.VOID);
+        Assertions.assertNotEquals(mo.getValue(), "methmain2");
+        Assertions.assertEquals(mo.getValue(), "methmain");
+    }
+
+    @Test
+    public void assignMethodDoesntExist() {
+        mem.getSymbolTable().newScope();
+        Assertions.assertThrows(StackException.class, () -> {
+            mem.assignValue("main", "methmain2");
+        });
+    }
+
+    @Test
+    public void assignMethodNotInitialised() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declMeth("main", null, ObjectType.VOID);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("main", "methmain");
+            Assertions.assertEquals(mem.getVal("main"), "methmain");
+        });
+    }
+
+    @Test
+    public void assignMethodWithNull() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declMeth("main", "methmain", ObjectType.VOID);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("main", null);
+        });
+    }
+
+    @Test
+    public void declTab() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("tab", 5, ObjectType.INT);
+        Assertions.assertEquals(mem.getVal("tab"), 0);
+        Assertions.assertEquals(mem.getNature("tab"), ObjectNature.TAB);
+        Assertions.assertEquals(mem.getType("tab"), ObjectType.INT);
+    }
+
+    @Test
+    public void declTabWithNotInteger() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        Assertions.assertThrows(java.lang.ClassCastException.class, () -> {
+            mem.declTab("tab", "notInteger", ObjectType.INT);
+        });
+    }
+
+    @Test
+    public void assignTab() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("tab", 5, ObjectType.INT);
+        mem.assignValue("tab", 0);
+        Map<Integer, List<HeapBlock>> blocks = mem.getHeap().getBlocks();
+        List<HeapBlock> block = blocks.get(8);
+        Assertions.assertEquals(block.get(0).getAddress(), 0);
+        Assertions.assertEquals(block.get(0).getSize(), 8);
+        Assertions.assertFalse(block.get(0).isFree());
+        Assertions.assertEquals(block.get(1).getAddress(), 8);
+        Assertions.assertEquals(block.get(1).getSize(), 8);
+        Assertions.assertTrue(block.get(1).isFree());
+    }
+
+    //TODO: revoir pour que ça lève la bonne exception surement avec Mock
+    @Test
+    public void assignTabWithNotInteger() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("tab", 5, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValue("tab", "notInteger");
+        });
+    }
+
+    @Test
+    public void removeDeclDoesntExist(){
+        mem.getSymbolTable().newScope();
+        Assertions.assertThrows(SymbolTableException.class, () -> {
+            mem.removeDecl("x");
+        });
+    }
+
+    @Test
+    public void removeDecl() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", 1, ObjectType.INT);
+        Assertions.assertEquals(mem.getVal("x"), 1);
+        Assertions.assertEquals(mem.getNature("x"), ObjectNature.VAR);
+        Assertions.assertEquals(mem.getStack().get(0), new MemoryObject("x", 1, ObjectNature.VAR, ObjectType.INT));
+        Assertions.assertEquals(mem.getType("x"), ObjectType.INT);
+        mem.removeDecl("x");
+        Assertions.assertThrows(SymbolTableException.class, () -> {
+            mem.getVal("x");
+        });
+        Assertions.assertTrue(mem.getStack().empty());
+    }
+
+    @Test
+    public void removeDeclTab() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("tab", 5, ObjectType.INT);
+        Map<Integer, List<HeapBlock>> blocks = mem.getHeap().getBlocks();
+        List<HeapBlock> block = blocks.get(8);
+        Assertions.assertEquals(block.get(0).getAddress(), 0);
+        Assertions.assertEquals(block.get(0).getSize(), 8);
+        Assertions.assertFalse(block.get(0).isFree());
+        Assertions.assertEquals(block.get(1).getAddress(), 8);
+        Assertions.assertEquals(block.get(1).getSize(), 8);
+        Assertions.assertTrue(block.get(1).isFree());
+        mem.removeDecl("tab");
+        blocks = mem.getHeap().getBlocks();
+        Assertions.assertTrue(blocks.get(8).isEmpty());
+        Assertions.assertTrue(blocks.get(16).isEmpty());
+        Assertions.assertTrue(blocks.get(32).isEmpty());
+        Assertions.assertTrue(blocks.get(64).isEmpty());
+        Assertions.assertTrue(blocks.get(128).isEmpty());
+        Assertions.assertTrue(blocks.get(256).isEmpty());
+        Assertions.assertTrue(blocks.get(512).isEmpty());
+        Assertions.assertTrue(blocks.get(1024).get(0).isFree());
+        Assertions.assertEquals(blocks.get(1024).get(0).getAddress(), 0);
+    }
+
+    @Test
+    public void assignValueArrayNotTab() throws SymbolTableException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declVar("x", 1, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValueArray("x", 0, 1);
+        });
+    }
+
+    @Test
+    public void assignValueArrayWithNotIntegerForIndex() throws SymbolTableException, StackException, HeapException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("x", 5, ObjectType.INT);
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            mem.assignValueArray("x", "1", 1);
+        });
+    }
+
+    //TODO: revoir ce test
+    @Test
+    public void assignValueArray() throws SymbolTableException, HeapException, StackException {
+        mem.getSymbolTable().newScope();
+        mem.declTab("x", 5, ObjectType.INT);
+        mem.assignValueArray("x", 0, 1);
     }
 
 }
